2482bb6e75b54d008af58a8b6c92d2d9af85ed64
Nico Wald <nicowald@mac.com>
2025-05-29 19:22:40 +0200

    MidiPipe V1.9.0

--- Casks/midipipe.rb
+++ Casks/midipipe.rb
@@ -1,8 +1,8 @@
 cask "midipipe" do
-    version "1.6.3"
-    sha256 "45d755aa39a16bb7d997f1114436c632f089308cb6531e1c5ac17e0d61f4c717"
+    version "1.9.0"
+    sha256 "09745b3eb24fa6045c4e9d143cd16ff069e95c4b8d63685fbae7c1cdd6462344"
   
-    url "http://www.subtlesoft.square7.net/data/homebrew/MidiPipe_V1.6.3.zip"
+    url "http://www.subtlesoft.square7.net/data/homebrew/MidiPipe_V1.9.0.zip"
     name "MidiPipe"
     desc "MidiPipe can be useful in a music studio or live on stage to route, map, filter, convert, display, input and output MIDI messages in real-time."
     homepage "http://www.subtlesoft.square7.net/"
