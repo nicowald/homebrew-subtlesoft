048953d6176191869972dc1311646c2086bd5e9b
Nico Wald <nicowald@mac.com>
2025-06-23 21:24:45 +0200

    Automated commit from Jenkins

--- Casks/midipipe.rb
+++ Casks/midipipe.rb
@@ -1,8 +1,8 @@
 cask "midipipe" do
-    version "1.10.0"
-    sha256 "e2b3815a9a257d8ec0771df549c976e7a0771b36de98a33cdefd539cdec08062"
+    version "2.0.0"
+    sha256 "a796daaf27b34574e22a4be7854f1139a0adc61ad39a0a3c6f38a008a668b4fe"
   
-    url "http://www.subtlesoft.square7.net/data/homebrew/MidiPipe_V1.10.0.zip"
+    url "http://www.subtlesoft.square7.net/data/homebrew/MidiPipe_V2.0.0.zip"
     name "MidiPipe"
     desc "MidiPipe can be useful in a music studio or live on stage to route, map, filter, convert, display, input and output MIDI messages in real-time."
     homepage "http://www.subtlesoft.square7.net/"
